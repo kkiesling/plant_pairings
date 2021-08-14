894bfcf9b1e2ddcc20f144b1bd99b23164c69d41
Kalin Kiesling <krkiesling@gmail.com>
2021-08-13 22:58:01 -0500

    initial script and data

---
+++ plot_network.py
@@ -0,0 +1,26 @@
+import pandas as pd
+import networkx as nx
+import matplotlib.pyplot as plt
+
+from pyvis.network import Network
+net = Network(height='100%', width='75%')
+
+df = pd.DataFrame()
+df = df.from_csv('./plant-pairing-data.csv')
+col_str = "ANISE HYSSOP,BIG BLUESTEM GRASS,BLUE WILD INDIGO,BOTTLE GENTIAN,BUTTERFLY WEED,BUTTON BLAZING STAR,CARDINAL FLOWER,COLUMBINE,COMMON BLUE VIOLET,COMMON MILKWEED,CULVERS ROOT,GARDEN PHLOX,GREAT BLUE LOBELIA,HAIRY BEARDTONGUE,JACK-IN-THE-PULPIT,JACOB'S LADDER,JOE PYE WEED,LEAD PLANT,LITTLE BLUESTEM GRASS,MICHIGAN LILY,MIDLAND SHOOTING STAR,MONKEY FLOWER,OBEDIENT PLANT,ORANGE CONEFLOWER,PRAIRIE BLAZING STAR,PRAIRIE SMOKE,PURPLE CONEFLOWER,PURPLE PRAIRIE CLOVER,RED/ROSE MILKWEED,RIDDELL'S GOLDENROD,ROSE MALLOW,ROYAL CATCHFLY,SILKY ASTER,SKY BLUE ASTER,SMOOTH ASTER,SMOOTH/MARSH PHLOX,SOLOMON'S PLUME,SWAMP ROSE MALLOW,SWEET BLACK EYED SUSAN,SWEET JOE PYE-WEED,TALL THIMBLEWEED,TRILLIUM,TURTLEHEAD,VIRGINIA BLUEBELL,WILD BERGAMOT,WILD BLUE PHLOX,WILD GERANIUM,WILD LUPINE"
+cols = col_str.split(',')
+df.columns = cols
+
+graph = nx.Graph()
+graph.add_nodes_from(cols)
+
+for planta in df.columns:
+    for plantb in cols:
+        if (df[planta][plantb] == 'x') and (planta != plantb):
+            print(planta, plantb)
+            graph.add_edge(planta, plantb)
+
+net.from_nx(graph)
+
+net.show_buttons()
+net.show('plant_network.html')
