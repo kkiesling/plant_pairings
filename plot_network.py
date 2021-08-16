aadb34070401b91e9ea624582ff2ff00d2a20e79
Kalin Kiesling <krkiesling@gmail.com>
2021-08-16 12:23:06 -0500

    well this doesn't actually go beyond page 2?

--- plot_network.py
+++ plot_network.py
@@ -70,9 +70,12 @@ def parse_plants(fname):
     return plant_list
 
 
-def collect_data(plant_list, all_plants, common):
+def collect_data(plant_list_init, all_plants, common):
     """scrape webiste for data from Prairie Moon Nursery"""
+    plant_list = [x.lower() for x in plant_list_init]  # make sure all lower case
+    print(plant_list)
 
+    # need this info so you don't get blocked by the website
     header = {'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) '
               'AppleWebKit/537.11 (KHTML, like Gecko) '
               'Chrome/23.0.1271.64 Safari/537.11',
@@ -82,37 +85,74 @@ def collect_data(plant_list, all_plants, common):
               'Accept-Language': 'en-US,en;q=0.8',
               'Connection': 'keep-alive'}
 
-    # scrape website
-
+    # base
     url_base = 'https://www.prairiemoon.com/seeds/'
 
+    # get name type being used
     if common:
         name_type = 'common-name'
     else:
         name_type = 'latin-name'
 
-    all_plants = []
+    # iterate over all "search" pages to get all plant names and URLs
+    # for plants in the list
+    all_plants = {}
+    # for i in range(2, 29):
+#
+    #     if i == 1:
+    #         url = url_base
+    #     else:
+    #         url = url_base + '?page={}'.format(i)
+#
+    #     # get webpage
+#
+    #     req = urllib.request.Request(url=url, headers=header)
+    #     page = urllib.request.urlopen(req).read().decode("utf-8")
+    #     htmltree = html.fromstring(page)
+    #     all_spans = htmltree.cssselect('span')
+    #     for span in all_spans:
+    #         # all_plants.append(span.find('result.name'))
+    #         span_str = html.tostring(span).decode("utf-8")
+    #         if name_type in span_str:
+    #             plant_name = span_str.split('>')[-2].split('<')[0]
+#
+    #             if plant_name in plant_list:
+    #                 # get URL so we can get to it later
+    #                 pass
 
-    for i in range(2, 29):
+    # iterate over all search result pages
+    for i in range(1, 29):
 
+        # get current url
         if i == 1:
             url = url_base
         else:
             url = url_base + '?page={}'.format(i)
 
         # get webpage
-
         req = urllib.request.Request(url=url, headers=header)
         page = urllib.request.urlopen(req).read().decode("utf-8")
         htmltree = html.fromstring(page)
-        all_spans = htmltree.cssselect('span')
-        for span in all_spans:
-            # all_plants.append(span.find('result.name'))
-            span_str = html.tostring(span).decode("utf-8")
-            if name_type in span_str:
-                plant_name = span_str.split('>')[-2].split('<')[0]
-                all_plants.append(plant_name)
 
+        # get all products on page
+        all_products = htmltree.find_class('category-product-name')
+        print('******* {}'.format(url))
+        # get product names and urls
+        for prod in all_products:
+            #print(html.tostring(prod))
+            # get plant name and url
+            url = prod.xpath('a/@href')
+            print(html.tostring(prod.find_class(name_type)
+                                [0]).decode("utf-8").split('>')[1].split('<')[0])
+            name = html.tostring(prod.find_class(name_type)
+                                 [0]).decode("utf-8").split('>')[1].split('<')[0]
+            #print(name.lower())
+            if name.lower() in plant_list:
+                # keep
+                #print(name)
+                all_plants[name] = url
+
+        htmltree.clear()
 
     print(all_plants)
 
@@ -123,8 +163,9 @@ def main():
     # args = args_parse()
 
     # plant_list = parse_plants(args.plantfile[0])
-
-    collect_data([], True, True)
+    col_str = "ANISE HYSSOP,BIG BLUESTEM GRASS,FALSE INDIGO,BOTTLE GENTIAN,BUTTERFLY WEED,BUTTON BLAZING STAR,CARDINAL FLOWER,COLUMBINE,COMMON BLUE VIOLET,COMMON MILKWEED,CULVERS ROOT,GARDEN PHLOX,GREAT BLUE LOBELIA,HAIRY BEARDTONGUE,JACK-IN-THE-PULPIT,JACOB'S LADDER,JOE PYE WEED,LEAD PLANT,LITTLE BLUESTEM GRASS,MICHIGAN LILY,MIDLAND SHOOTING STAR,MONKEY FLOWER,OBEDIENT PLANT,ORANGE CONEFLOWER,PRAIRIE BLAZING STAR,PRAIRIE SMOKE,PURPLE CONEFLOWER,PURPLE PRAIRIE CLOVER,RED/ROSE MILKWEED,RIDDELL'S GOLDENROD,ROSE MALLOW,ROYAL CATCHFLY,SILKY ASTER,SKY BLUE ASTER,SMOOTH ASTER,SMOOTH/MARSH PHLOX,SOLOMON'S PLUME,SWAMP ROSE MALLOW,SWEET BLACK EYED SUSAN,SWEET JOE PYE-WEED,TALL THIMBLEWEED,TRILLIUM,TURTLEHEAD,VIRGINIA BLUEBELL,WILD BERGAMOT,WILD BLUE PHLOX,WILD GERANIUM,WILD LUPINE"
+    cols = col_str.split(',')
+    collect_data(cols, True, True)
 
     # colled data
 
